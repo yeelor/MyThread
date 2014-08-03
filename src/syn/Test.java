f08b6ed879f489395ca548ab30a2c0e900629862
aleak <aleak@aleak.tk>
2014-08-03 22:21:29 +0800

    题目３

--- src/syn/Test.java
+++ src/syn/Test.java
@@ -1,85 +1,87 @@
 package syn;
+
 import java.util.concurrent.CopyOnWriteArrayList;
 
-
 //不能改动此Test类	
-public class Test extends Thread{
-	
+public class Test extends Thread {
+
 	private TestDo testDo;
 	private String key;
 	private String value;
-	
-	public Test(String key,String key2,String value){
+
+	public Test(String key, String key2, String value) {
 		this.testDo = TestDo.getInstance();
-		/*常量"1"和"1"是同一个对象，下面这行代码就是要用"1"+""的方式产生新的对象，
-		以实现内容没有改变，仍然相等（都还为"1"），但对象却不再是同一个的效果*/
-//		this.key = key;//key+key2; 
-		this.key = key+key2; 
+		/*
+		 * 常量"1"和"1"是同一个对象，下面这行代码就是要用"1"+""的方式产生新的对象，
+		 * 以实现内容没有改变，仍然相等（都还为"1"），但对象却不再是同一个的效果
+		 */
+		// this.key = key;//key+key2;
+		this.key = key + key2;
 		this.value = value;
-		
-		
-//		String a = "1"+"";//编译器优化，是同一个对象
-//		String b = "1"+"";
-//		String c = "1";
+
+		// String a = "1"+"";//编译器优化，是同一个对象
+		// String b = "1"+"";
+		// String c = "1";
 	}
 
-
-	public static void main(String[] args) throws InterruptedException{
-		Test a = new Test("1","","1");
-		Test b = new Test("1","","2");
-		Test c = new Test("3","","3");
-		Test d = new Test("4","","4");
-		System.out.println("begin:"+(System.currentTimeMillis()/1000));
+	public static void main(String[] args) throws InterruptedException {
+		Test a = new Test("1", "", "1");
+		Test b = new Test("1", "", "2");
+		Test c = new Test("3", "", "3");
+		Test d = new Test("4", "", "4");
+		System.out.println("begin:" + (System.currentTimeMillis() / 1000));
 		a.start();
 		b.start();
 		c.start();
 		d.start();
 
 	}
-	
-	public void run(){
+
+	@Override
+	public void run() {
 		testDo.doSome(key, value);
 	}
 }
 
 class TestDo {
 
-	private TestDo() {}
-	private static TestDo _instance = new TestDo();	
+	private TestDo() {
+	}
+
+	private static TestDo _instance = new TestDo();
+
 	public static TestDo getInstance() {
 		return _instance;
 	}
 
-//	private ArrayList keys = new ArrayList();
+	// private ArrayList keys = new ArrayList();
 	private CopyOnWriteArrayList keys = new CopyOnWriteArrayList();
-	
+
 	public void doSome(Object key, String value) {
-		Object o  = key;
-		if(!keys.contains(key)){
+		Object o = key;
+		if (!keys.contains(key)) {
 			keys.add(o);
-		}else{
-			for(Object oo:keys){
+		} else {
+			for (Object oo : keys) {
 				try {
 					Thread.sleep(200);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				if(oo.equals(o)){
-					o=oo;
+
+				if (oo.equals(o)) {
+					o = oo;
 					break;
 				}
 			}
 		}
-		
+
 		synchronized (o)
 		// 以大括号内的是需要局部同步的代码，不能改动!
 		{
 			try {
 				Thread.sleep(1000);
-				System.out.println(key+":"+value + ":"
-						+ (System.currentTimeMillis() / 1000));
+				System.out.println(key + ":" + value + ":" + (System.currentTimeMillis() / 1000));
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
